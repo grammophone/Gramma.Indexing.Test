0d83f5581368f394d215e3d53468502d470eb9ae
grammophone <emmanuel.liossis@gmail.com>
2016-03-29 06:31:53 +0300

    Change assembly name to Gramma.Indexing.Test

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SuffixTreeTest.Properties {
+namespace Gramma.Indexing.Test.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
